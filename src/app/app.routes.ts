dfb48d7d26d779a0841fba6a071571118dbbd2a6
Grudged <45639954+Grudged@users.noreply.github.com>
2025-05-16 14:03:58 -0700

    change redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,7 +14,7 @@ export const routes: Routes = [
   { path: 'artwork', component: SketchesComponent }, // Show sketches for artwork
   { path: 'contact', component: ContactComponent }, // Contact page
   // ...other routes
-  { path: '', redirectTo: '/index.html', pathMatch: 'full' }, // default route
+  { path: '', redirectTo: 'path', pathMatch: 'full' }, // default route
 ];
 
 @NgModule({
