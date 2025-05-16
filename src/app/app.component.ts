5f4e4845d518eb9bdfa575b928438e5d4a016a37
Grudged <45639954+Grudged@users.noreply.github.com>
2025-05-16 15:38:49 -0700

    trying to remove footer from other tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AboutAuthorComponent } from "./about-author/about-author.component";
 import { InTheWorksComponent } from "./in-the-works/in-the-works.component";
@@ -8,10 +9,21 @@ import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Olli-Project';
+  router: any;
+
+  constructor() {
+    console.log('AppComponent initialized');
+    this.router = {
+      navigate: (path: string) => {
+        console.log(`Navigating to ${path}`);
+      }
+    };
+    console.log(this.router);
+  }
 }
