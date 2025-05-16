0948cba79275aff5155377128db3c87fd7f35544
Grudged <45639954+Grudged@users.noreply.github.com>
2025-05-15 20:34:34 -0700

    latest version 5/15

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutAuthorComponent } from './about-author/about-author.component';
+import { InTheWorksComponent } from './in-the-works/in-the-works.component';
+import { SketchesComponent } from './sketches/sketches.component';
+// ...import other components
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: AboutAuthorComponent },
+  { path: 'about', component: HomeComponent },
+  { path: 'in-the-works', component: InTheWorksComponent },
+  { path: 'artwork', component: SketchesComponent }, // Show sketches for artwork
+  // ...other routes
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // default route
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
