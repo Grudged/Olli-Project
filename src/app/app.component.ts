45e167eff8c52ea3733ba8928dd6119e9e465d34
Grudged <45639954+Grudged@users.noreply.github.com>
2025-05-17 22:45:31 -0700

    more updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,12 +16,38 @@ import { HeaderComponent } from "./header/header.component";
 export class AppComponent {
   title = 'Olli-Project';
   currentUrl: any;
+  backgroundStyle: any = {};
 
   constructor(private router: Router) {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         this.currentUrl = event.url;
-        console.log('Current URL:', this.currentUrl);
+        // Set background image based on route
+        if (this.currentUrl === '/about') {
+          this.backgroundStyle = {
+            'background-image': 'url(assets/about.jpg)',
+            'background-size': 'cover',
+            'background-position': 'center',
+            'position': 'fixed',
+            'top': '0',
+            'left': '0',
+            'width': '100vw',
+            'height': '100vh',
+            'z-index': '-1'
+          };
+        } else {
+          this.backgroundStyle = {
+            'background-image': 'url(assets/whimsy.jpg)',
+            'background-size': 'cover',
+            'background-position': 'center',
+            'position': 'fixed',
+            'top': '0',
+            'left': '0',
+            'width': '100vw',
+            'height': '100vh',
+            'z-index': '-1'
+          };
+        }
       }
     });
   }
