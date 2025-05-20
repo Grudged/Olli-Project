5a9303a5c15695964eb4371333dfca4bb12c7ca2
Grudged <45639954+Grudged@users.noreply.github.com>
2025-05-20 09:44:35 -0700

    info

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  public isLoggedIn: boolean = localStorage.getItem('isLoggedIn') === 'true';
 
 }
